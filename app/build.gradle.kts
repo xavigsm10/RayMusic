121acfbb6544ff8836b9526e201fb4024bc505b4
LebedAlIv2601 <lebedaliv2601@mail.ru>
2025-11-27 22:45:44 +0300

    kmp start infrastructure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -6,41 +8,26 @@ plugins {
 
 android {
     namespace = "com.mrtdk.liquid_glass"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.mrtdk.liquid_glass"
         minSdk = 21
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "0.1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-    buildFeatures {
-        compose = true
+    kotlin {
+        compilerOptions.jvmTarget = JvmTarget.JVM_11
     }
 }
 
 dependencies {
-
     implementation(project(":glass"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
---
+++ glass/src/commonMain/kotlin/com/mrtdk/glass/CommonStubClass.kt
@@ -0,0 +1,3 @@
+package com.mrtdk.glass
+
+class CommonStubClass
