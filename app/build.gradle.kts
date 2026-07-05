35e96e11de87fc4e6f3c878c2b7f411dc1739d28
xavigsm10 <xavigsm10@users.noreply.github.com>
2026-07-04 20:11:53 -0400

    actualizando version a 0.6.4 y agregando paypal

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.mrtdk.liquid_glass"
         minSdk = 23
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.6.3"
+        versionCode = 6
+        versionName = "0.6.4"
     }
 
     signingConfigs {
